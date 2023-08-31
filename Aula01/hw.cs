3dc9c9f029316d9ca1da7b0eba497b2db419dc6b
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-08-31 17:51:31 -0300

    Adicionando mono a máquina

--- Aula01/hw.cs
+++ Aula01/hw.cs
@@ -3,7 +3,7 @@ using System;
 namespace HelloWorld{
     class Program{
         static void Main(string[] args){
-            System.Console.WriteLine('Hello World!');
+            System.Console.WriteLine("Hello World!");
         }
     }
 }

4299690b79341082f9982d29ecd9145a7cc38f49
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2023-10-11 11:03:04 -0300

    Método para inserir no início e no fim

--- Aula11/listaligada.java
+++ Aula11/listaligada.java
@@ -1,19 +1,31 @@
 class Programa{
     public static void main(String args[]){
-        Lista x = new Lista();
-        x.Inserir("A");
-        x.Inserir("B");
-        x.Inserir("C");
-        x.Inserir("D");
-        x.Inserir("E");
-        System.out.println(x.Listar());
+        Lista numeros = new Lista();
+        numeros.InserirInicio("1");
+        numeros.InserirInicio("2");
+        numeros.InserirInicio("3");
+        System.out.println(numeros.Listar());
+        Lista texto = new Lista();
+        texto.InserirFim("Ester");
+        texto.InserirFim("Oliveira");
+        texto.InserirFim("Melo");
+        System.out.println(texto.Listar());
     }
 }
 
 
 class Lista{
     private Node inicio = null, fim = null;
-    public void Inserir(String valor){
+    public void InserirInicio(String valor){
+        Node novo = new Node();
+        novo.valor = valor;
+        novo.prox = inicio;
+        inicio = novo;
+        if(fim == null){
+            fim = novo;
+        }
+    }
+    public void InserirFim(String valor){
         Node novo = new Node();
         novo.valor = valor;
         if (inicio == null){

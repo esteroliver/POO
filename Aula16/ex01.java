b5e83a89eb7bf871aa5169a346c766608c23b6f9
ESTER OLIVEIRA DO NASCIMENTO DUARTE MELO <20231014040002@ifrn.local>
2023-10-26 11:53:56 -0300

    implementar comparação para ordenar os dias (incompleto)

--- Aula16/ex01.java
+++ Aula16/ex01.java
@@ -6,35 +6,21 @@ class Programa {
     public static void main(String args[]){
         IPessoa a1 = new Aluno("Ester", "TADS", LocalDateTime.parse("2004-09-17T00:00:00"));
         IPessoa p1 = new Professor("Eduardo", "Diatinf", LocalDateTime.parse("1971-01-01T00:00:00"));
-        
-        System.out.println(a1.getNome());
-        System.out.println(a1.getNascimento());
-        System.out.println(p1.getNome());
-        System.out.println(p1.getNascimento());
 
         Aluno a2 = new Aluno("Débora", "TADS", LocalDateTime.parse("2001-11-21T00:00:00"));
         Professor p2 = new Professor("Jorgiano", "Diatinf", LocalDateTime.parse("1970-11-20T00:00:00"));
-
-        System.out.println(a2.getNome());
-        System.out.println(a2.getCurso());
-        System.out.println(a2.getNascimento());
-        System.out.println(p2.getNome());
-        System.out.println(p2.getDiretoria());
-        System.out.println(p2.getNascimento());
 
         IPessoa[] v = {a1, a2, p1, p2};
         mesAniversariantes(v, 11);
 
         IPessoa[] participantes = {a1, a2, p1, p2};
         mostrarParticipantes(participantes);
-
-
     }
 
     public static void mesAniversariantes(IPessoa[] pessoas, int mes){
         for (IPessoa p : pessoas) {
             if(p.getNascimento().getMonthValue() == mes){
-                System.out.print(p.getNome());
+                System.out.print(p.getNome() + " " + p.getNascimento());
                 System.out.println();
             }
         }
@@ -53,7 +39,7 @@ class Programa {
 interface IPessoa extends Comparable{
     String getNome();
     LocalDateTime getNascimento();
-    
+    int getDia();
 }
 
 class Aluno implements IPessoa{
@@ -64,6 +50,13 @@ class Aluno implements IPessoa{
 
     public String getNome() { return nome;}
     public LocalDateTime getNascimento() { return datanasc; }
+    public int getDia() {
+        int dia;
+        DateTimeFormatter formatacao = DateTimeFormatter.ofPattern("dd");
+        String dia_str = datanasc.format(formatacao);
+        dia = Integer.parseInt(dia_str);
+        return dia;
+    }
     public String getCurso() { return curso; }
     public int compareTo(Object ob){
         IPessoa x = (IPessoa) ob;
@@ -93,6 +86,13 @@ class Professor implements IPessoa{
 
     public String getNome() { return nome;}
     public LocalDateTime getNascimento() { return datanasc; }
+    public int getDia() {
+        int dia;
+        DateTimeFormatter formatacao = DateTimeFormatter.ofPattern("dd");
+        String dia_str = datanasc.format(formatacao);
+        dia = Integer.parseInt(dia_str);
+        return dia;
+    }
     public String getDiretoria() { return diretoria; }
     public int compareTo(Object ob){
         IPessoa x = (IPessoa) ob;

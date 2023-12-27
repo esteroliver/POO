cc5e733af245e2098ad4cbeec540688a86ecdf9e
Ester Oliveira <oliver.ester.2004@gmail.com>
2023-12-27 20:47:13 -0300

    crud da classe

--- serializacao/laboratorio.java
+++ serializacao/laboratorio.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Laboratorio{
-    private int id;
-    private String descricao;
+    public int id;
+    public String descricao;
 
     public String toString(){
         return id + '\n' + descricao;
@@ -38,5 +38,47 @@ class NLaboratorio{
         catch (FileNotFoundException ex){
 
         }
+    }
+
+    public void Inserir(Laboratorio l){
+        fromXml();
+        int id = 0;
+        for (Laboratorio la : labs) {
+            if(la.id > id) id = la.id;
+        }
+        l.id = id+1;
+        labs.add(l);
+        toXml(labs);
+    }
+
+    public List<Laboratorio> Listar(){
+        fromXml();
+        return labs;
+    }
+
+    public Laboratorio obterId(int id){
+        fromXml();
+        for (Laboratorio la : labs) {
+            if (la.id == id) return la;
+        }
+    }
+
+    public void Atualizar(Laboratorio l){
+        fromXml();
+        Laboratorio l_subs = obterId(l.id);
+        if (l_subs != null){
+            labs.remove(l_subs);
+            labs.add(l);
+        }
+        toXml(labs);
+    }
+
+    public void Excluir(Laboratorio l){
+         fromXml();
+        Laboratorio l_subs = obterId(l.id);
+        if (l_subs != null){
+            labs.remove(l_subs);
+        }
+        toXml(labs);
     }
 }
